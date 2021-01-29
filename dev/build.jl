b4ff89de9a241b5a351de6de4a6fead01e2976a9
Documenter.jl <documenter@juliadocs.github.io>
2021-01-29 12:31:38 +0000

    build based on 6bc7bf9

---
+++ dev/build.jl
@@ -0,0 +1,29 @@
+# This file is meant to circumvent a Documenter.jl eval issue, see
+# https://github.com/JuliaDocs/Documenter.jl/issues/1514. 
+
+using Turing
+using TuringPlots
+T = TuringPlots
+
+turingplot_code = raw"""
+using Turing
+using TuringPlots
+
+@model function binom(n, k)
+   θ ~ Beta(1, 1)
+   k ~ Binomial(n, θ)
+   return k, θ
+end
+chains = sample(binom(9, 6), NUTS(0.65), 1000)
+plot(chains, y = :θ)
+"""
+
+function turingplot_eval() 
+    println("Evaluating turingplot")
+    p = eval(Meta.parse("begin $turingplot_code end"))
+    T.write_svg("docs/build/turingplot.svg", p)
+end
+
+function build()
+    turingplot_eval()
+end
