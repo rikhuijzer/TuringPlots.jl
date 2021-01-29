8c530ef0e843b88586583b118438ee54311413ce
Documenter.jl <documenter@juliadocs.github.io>
2021-01-29 19:08:07 +0000

    build based on d77146f

--- dev/build.jl
+++ dev/build.jl
@@ -1,31 +1,9 @@
-# This file is meant to circumvent a Documenter.jl eval issue, see
-# https://github.com/JuliaDocs/Documenter.jl/issues/1514. 
-
 using Turing
-using TuringPlots
-T = TuringPlots
-
-turingplot_code = raw"""
-using Gadfly
-using Turing
-using TuringPlots
 
 @model function binom(n, k)
-   θ ~ Beta(1, 1)
-   k ~ Binomial(n, θ)
-   return k, θ
+    α ~ Beta(1, 1)
+    θ ~ Beta(α, 1)
+    k ~ Binomial(n, θ)
+    return k, θ
 end
-
-chains = sample(binom(9, 6), NUTS(0.65), 1000)
-plot(chains, x = :θ, Geom.density, Guide.ylabel("Density"))
-"""
-
-function turingplot_eval() 
-    println("Evaluating turingplot")
-    p = eval(Meta.parse("begin $turingplot_code end"))
-    T.write_svg("docs/build/turingplot.svg", p)
-end
-
-function build()
-    turingplot_eval()
-end
+const chn = sample(binom(9, 6), NUTS(0.65), MCMCThreads(), 1000, 3)
