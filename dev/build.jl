2418df14b39868cf1ea5ed7ec13a12c831abbabf
Documenter.jl <documenter@juliadocs.github.io>
2021-02-18 14:16:57 +0000

    build based on 07f02d4

--- dev/build.jl
+++ dev/build.jl
@@ -5,8 +5,18 @@ using Turing
     α ~ Beta(1, 1)
     θ ~ Beta(α, 1)
     k ~ Binomial(n, θ)
-    return k, θ
+    k, θ
 end
 
 Random.seed!(123)
 chn = sample(binom(9, 6), NUTS(0.65), MCMCThreads(), 1000, 3)
+
+@model function binom2(n, k)
+    α ~ Beta(2, 1)
+    θ ~ Beta(α, 1)
+    k ~ Binomial(n, θ)
+    k, θ
+end
+
+Random.seed!(123)
+chn2 = sample(binom(5, 3), NUTS(0.65), MCMCThreads(), 1000, 3)
